56126ba4756d88a7a210743d07e07a3ba190b7eb
davodroid <davodroid@16f77f2e-bb1b-4951-a0ee-c5cc7ba034af>
2016-03-27 19:25:23 +0000

    Fixed missing options in measure select
    git-svn-id: https://vip-review-svn.wordpress.com/trials/davodroid@19786 16f77f2e-bb1b-4951-a0ee-c5cc7ba034af

--- drafts-for-friends/views/measure-select.php
+++ drafts-for-friends/views/measure-select.php
@@ -1,7 +1,7 @@
 <input name="expires" type="text" value="2" size="4"/>
 <select name="measure">
-	<option value="s"><?php __( 'seconds', 'draftsforfriends' ); ?></option>
-	<option value="m"><?php __( 'minutes', 'draftsforfriends' ) ?></option>
-	<option value="h" selected="selected"><?php __( 'hours', 'draftsforfriends' ); ?></option>
+	<option value="s"><?php echo __( 'seconds', 'draftsforfriends' ); ?></option>
+	<option value="m"><?php echo __( 'minutes', 'draftsforfriends' ) ?></option>
+	<option value="h" selected="selected"><?php echo __( 'hours', 'draftsforfriends' ); ?></option>
 	<option value="d"><?php echo __( 'days', 'draftsforfriends' ); ?></option>
 </select>
